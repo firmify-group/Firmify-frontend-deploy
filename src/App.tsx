ff40b7ffdab53cc52de5e34e58971e51d16a5c1d
Claudio Espinoza <claudioyugen@gmail.com>
2025-05-29 14:24:13 -0400

    refactor(config): cambia la ubicación del css main with tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css';
+import './assets/css/App.css';
 
 const App = () => {
 	return (
-		<div className="content">
-			<h1>Rsbuild with React</h1>
+		<div className="bg-primary-400">
+			<h1 className="">Rsbuild with React</h1>
 			<p>Start building amazing things with Rsbuild.</p>
 		</div>
 	);
