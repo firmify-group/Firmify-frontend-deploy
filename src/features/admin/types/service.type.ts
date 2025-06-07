94d380fa092ad625a84630ec68431c8fc37e5741
Claudio Espinoza <claudioyugen@gmail.com>
2025-06-07 17:02:03 -0400

    feat(frontend): agrega elementos de tipado para la pagina de inicio

---
+++ src/features/admin/types/service.type.ts
@@ -0,0 +1,19 @@
+export type SummaryRequest = {
+    status: boolean;
+    data: {
+        request: {
+            total_resueltos: number;
+            total_pendientes: number;
+            total_objeciones: number;
+            total_procesos: number;
+        };
+        category_sumers: {
+            name: string;
+            total: number;
+        }[];
+    };
+    message: string;
+    timestamp: string;
+};
+
+
