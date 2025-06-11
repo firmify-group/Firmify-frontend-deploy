3bb2c19622d173613c5c12ff315ae271638b1a90
Claudio Espinoza <claudioyugen@gmail.com>
2025-06-10 21:17:03 -0400

    feat(frontend): agrega logica de validación

--- src/features/public/components/molecules/FormLogin.tsx
+++ src/features/public/components/molecules/FormLogin.tsx
@@ -1,25 +1,44 @@
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 import { NavLink } from 'react-router';
+import type { Slot } from '@feature/public/types/login.types';
+import { useState } from 'react';
 
-type MonitoringSummaryProps = {
-	children: React.ReactNode;
-};
+const FormLogin: React.FC<Slot> = ({ children }) => {
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState<string>();
 
-const FormLogin: React.FC<MonitoringSummaryProps> = ({ children }) => {
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setLoading(true);
+		setError('Sexo erroneo');
+
+		const formData = new FormData(e.currentTarget);
+		const email = formData.get('email');
+		const password = formData.get('password');
+
+		// TODO: Implementa la lógica de autenticación aquí
+		console.log(formData, 'password:', password, 'Email:', email);
+	};
+
 	return (
 		<form
 			className="flex flex-col gap-10 w-full h-fit **:body-1"
-			onSubmit={(e) => e.preventDefault()}
+			onSubmit={handleSubmit}
 			aria-labelledby="login-heading"
 		>
 			<section className="w-full h-fit flex flex-col gap-4 ">{children}</section>
-			<button
-				type="submit"
-				className="button button-primary-IDLE"
-				aria-describedby="login-description"
-			>
-				Iniciar sesión
-			</button>
+
+			<div className="flex flex-col gap-2 w-full h-16">
+				<button
+					type="submit"
+					className="button button-primary-IDLE"
+					aria-describedby="login-description"
+					disabled={loading}
+				>
+					{loading ? 'Cargando...' : 'Iniciar sesión'}
+				</button>
+				<small>{error}</small>
+			</div>
 
 			<footer className="flex gap-2 h-24 items-end">
 				<NavLink to={'/'} className="flex items-center gap-2">
--- src/features/public/types/login.types.ts
+++ src/features/public/types/login.types.ts
@@ -1,10 +1,14 @@
 export type InputsFormType = {
-	name: string;
-	label: string;
-	type: string;
-	placeholder: string;
-	pattern: string;
-	maxLength: number;
-	minLength: number;
-	alert: string;
+    name: string;
+    label: string;
+    type: string;
+    placeholder: string;
+    pattern: string;
+    maxLength: number;
+    minLength: number;
+    alert: string;
+};
+
+export type Slot = {
+    children: React.ReactNode;
 };
